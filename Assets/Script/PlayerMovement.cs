56367055d857cb7d6caf68773a533bd10d0566a0
Rolwen <n.rnolwen@gmail.com>
2024-09-10 00:27:56 +0200

    Player can run

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,31 +6,39 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float walkSpeed = 6f;
+    public float walkSpeed = 2f;
+    public float runSpeed = 12f;
+    public float jumpForce = 2f;
     Vector3 movement;
     Vector3 jump;
-     public float jumpForce = 2f;
 
     [SerializeField] private Rigidbody _rigidbody;
 
-    private bool _isGrounded;
     
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        jump = new Vector3(0, 2.0f, 0);
+        jump = new Vector3(0, 1f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
 
-        movement = new Vector3(moveX, 0f, moveY) * walkSpeed * Time.deltaTime;
+        if (IsRunning() && IsGrounded())
+        {
+            movement = new Vector3(moveX, 0f, moveY) * runSpeed * Time.deltaTime;
+        }
+        else{
+            movement = new Vector3(moveX, 0f, moveY) * walkSpeed * Time.deltaTime;
+        }
 
+        // we take current position of the player + add the movement
         transform.position += movement;
 
         if (Input.GetKeyDown(KeyCode.Space) & IsGrounded())
@@ -43,12 +51,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump(){
 
-       
         _rigidbody.AddForce(jump * jumpForce, ForceMode.Impulse);
     }
 
     bool IsGrounded()
     {
         return _rigidbody.velocity.y == 0;
+    }
+
+    bool IsRunning()
+    {
+        if(Input.GetKey(KeyCode.LeftShift)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
