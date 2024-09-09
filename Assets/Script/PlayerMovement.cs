10d7b404351185b5c7393b440b7511b1280df6f5
Rolwen <n.rnolwen@gmail.com>
2024-09-10 01:40:34 +0200

    Started Character Animation (Idle works)

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -11,24 +11,38 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 2f;
     Vector3 movement;
     Vector3 jump;
-
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] Animator _playerAnim;
 
+    void Awake()
+    {
+        _playerAnim = GetComponentInChildren<Animator>();
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         jump = new Vector3(0, 1f, 0);
+        //_playerAnim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
+        _playerAnim.SetBool("Walk",false);
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+
+        // if this, it means the user is pressing the moving button
+        if (moveX != 0 || moveY != 0)
+        {
+            _playerAnim.SetBool("Walk", true); 
+        }
+        else
+        {
+            _playerAnim.SetBool("Walk", false);
+        }
 
         if (IsRunning() && IsGrounded())
         {
@@ -43,8 +57,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) & IsGrounded())
         {
+            _playerAnim.SetBool("Walk",true);
             Jump();
         }
+
+        
 
 
     }
