2e5cd2c1d95361d26fd89c72b2b56fb98056e86c
Rolwen <n.rnolwen@gmail.com>
2024-09-13 11:58:33 +0200

    Ca marche right

--- Assets/Script/FloatingHealthBar.cs
+++ Assets/Script/FloatingHealthBar.cs
@@ -1,16 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FloatingHealthBar : MonoBehaviour
 {
-    [SerializeField] public Slider slider;
-    
+    public Slider slider;
+    public Image fillImage;
 
-    public void UpdateHealthBar(float currentValue, float maxValue){
-
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
         slider.value = currentValue / maxValue;
+        if (currentValue == 40)
+        {
+            fillImage.color = Color.Lerp(Color.yellow, Color.red, 0.5f); 
+        }
+        else if (currentValue == 20)
+        {
+            fillImage.color = Color.red;
+        }
+        else
+        {
+            fillImage.color = Color.green;
+        }
     }
 }
